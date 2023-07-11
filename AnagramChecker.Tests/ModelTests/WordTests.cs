e83e408217694abc0a7a8c681bcc38a4c06587a7
Lindsay Warr <iamalion@gmail.com>
2023-07-11 10:59:06 -0700

    Pass word constructor test

--- AnagramChecker.Tests/ModelTests/WordTests.cs
+++ AnagramChecker.Tests/ModelTests/WordTests.cs
@@ -6,7 +6,12 @@ namespace AnagramChecker.Tests
   [TestClass]
   public class WordTests
   {
-    
+    [TestMethod]
+    public void WordConstructor_CreatesInstanceOfWord_Word()
+    {
+        Word newWord = new Word();
+        Assert.AreEqual(typeof(Word), newWord.GetType());
+    }
   }
   
 }
--- AnagramChecker/Models/Word.cs
+++ AnagramChecker/Models/Word.cs
@@ -1,9 +1,11 @@
-using System;
-
 namespace AnagramChecker.Models
 {
     public class Word 
     {
+        public Word()
+        {
+            
+        }
     
     }
 }
