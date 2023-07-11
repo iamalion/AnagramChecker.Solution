56f327c0c1998ad40c6ba0c803cb50b9dd3ffdf0
Lindsay Warr <iamalion@gmail.com>
2023-07-11 11:48:08 -0700

    Pass second test to return anagram or not anagram

--- AnagramChecker/Models/Word.cs
+++ AnagramChecker/Models/Word.cs
@@ -1,11 +1,20 @@
+using System;
+using System.Linq;
+
 namespace AnagramChecker.Models
 {
     public class Word 
     {
-        public Word()
+        public bool IsAnagram(string inputtedWordOne, string inputtedWordTwo)
         {
+            char[] wordOneChars = inputtedWordOne.ToLower().ToCharArray();
+            char[] wordTwoChars = inputtedWordTwo.ToLower().ToCharArray();
+
+            Array.Sort(wordOneChars);
+            Array.Sort(wordTwoChars);
+
+            return wordOneChars.SequenceEqual(wordTwoChars);
             
         }
-    
     }
 }
--- AnagramChecker/Program.cs
+++ AnagramChecker/Program.cs
@@ -1,12 +1,40 @@
 using System;
+using AnagramChecker.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AnagramChecker
 {
     class Program
-    {
+    {   
         static void Main()
         {
-            
+            Console.WriteLine("Enter a word:");
+            string wordOne = Console.ReadLine();
+            Console.WriteLine("Enter a second word to check if it's an anagram of the first:");
+            string wordTwo = Console.ReadLine();
+
+            bool result = IsAnagram(wordOne, wordTwo);
+
+            if (result)
+            {
+                Console.WriteLine($"Hooray! '{wordOne}' and '{wordTwo}' are anagrams!");
+            } 
+            else{
+                Console.WriteLine($"Ah shucks. '{wordOne}' and '{wordTwo}' are not anagrams.");
+            }
+            Console.ReadLine();
+        }
+        static bool IsAnagram(string wordOne, string wordTwo)
+        {
+            char[] wordOneChars = wordOne.ToLower().ToCharArray();
+            char[] wordTwoChars = wordTwo.ToLower().ToCharArray();
+
+            Array.Sort(wordOneChars);
+            Array.Sort(wordTwoChars);
+
+            return Enumerable.SequenceEqual(wordOneChars, wordTwoChars);
+
         }
     }
 }
