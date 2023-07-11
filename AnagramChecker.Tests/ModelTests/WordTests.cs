87f6b2a7d6e697a7eaa15088b39fa2a2aba2de1f
Lindsay Warr <iamalion@gmail.com>
2023-07-11 11:52:27 -0700

    Pass second test to return anagram or not anagram

--- AnagramChecker.Tests/ModelTests/WordTests.cs
+++ AnagramChecker.Tests/ModelTests/WordTests.cs
@@ -12,6 +12,17 @@ namespace AnagramChecker.Tests
         Word newWord = new Word();
         Assert.AreEqual(typeof(Word), newWord.GetType());
     }
+    [TestMethod]
+    public void IsAnagram()
+    {
+        string inputtedWordOne = "beard";
+        string inputtedWordTwo = "bread";
+        Word word = new Word();
+
+        bool result = word.IsAnagram(inputtedWordOne, inputtedWordTwo);
+
+        Assert.IsTrue(result);
+    }
   }
   
 }
